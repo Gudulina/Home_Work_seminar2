0bcff2e2c385d1449070a09304f624a822d14b29
Elena <gudulina@mail.ru>
2022-05-21 00:22:38 +0700

    Add first task

---
+++ First task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Function (int num)
+{
+    return num / 10 % 10;
+}
+
+Console.WriteLine ("Введите трёхзначное число");
+string s_n = Console.ReadLine ();
+int n = int.Parse (s_n);
+
+Console.WriteLine (Function (n));
