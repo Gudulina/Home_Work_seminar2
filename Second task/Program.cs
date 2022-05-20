0d0e429e01e4b0183b79d67cb0a68b5418768c38
Elena <gudulina@mail.ru>
2022-05-21 01:21:31 +0700

    Add second task

---
+++ Second task/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void Function ()
+{
+    int number = new Random ().Next(100, 1000);
+    Console.WriteLine($"{number} --> {number / 100}{number % 10}");
+}
+
+Function();
